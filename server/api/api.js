4b5cd433f8432e625564898a93523ad275f60088
Bakhti Rasulov <rasulovbahti@gmail.com>
2025-01-26 02:35:31 -0500

    [server]: transformed server into js for vercel deployment

--- server/api/api.js
+++ server/api/api.js
@@ -1,6 +1,5 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
 import AWS from 'aws-sdk';
 import multer from 'multer';
 import { v4 as uuidv4 } from 'uuid';
@@ -14,7 +13,7 @@ const s3 = new AWS.S3({
   region: process.env.NODE_AWS_S3_REGION
 });
 
-const CLIENT_ADDRESS = process.env.NODE_CLIENT_ADDRESS!;
+const CLIENT_ADDRESS = process.env.NODE_CLIENT_ADDRESS;
 
 export const upload = multer({
   storage: multer.memoryStorage(),
@@ -24,13 +23,13 @@ export const upload = multer({
   fileFilter: (req, file, cb) => {
     if (file.mimetype.startsWith('image/')) {
       cb(null, true);
-    } else {
+    }
+    else {
       cb(new Error('Invalid file type. If uploading a file, please upload an image.'));
     }
   }
 });
-
-function hash(passcode: string) {
+function hash(passcode) {
   return argon2.hash(passcode, {
     type: argon2.argon2id,
     memoryCost: 65536, // 64MB in KiB
@@ -38,114 +37,79 @@ function hash(passcode: string) {
     parallelism: 4 // degree of parallelism
   });
 }
-
-export const createGroup = async (req: any, res: any) => {
+export const createGroup = async (req, res) => {
   const { group_name, email, passcode } = req.body;
-
   if (!group_name || !email || !passcode) {
     return res.status(400).json({ error: 'Group name, email, and password are required' });
   }
-  
   const group_id = uuidv4().split('-')[0]; // Takes first segment of UUID (8 characters)
   const group_url = `${CLIENT_ADDRESS}/timeline/${group_id}`;
-
   console.log(`Creating group with id: ${group_id} and name: ${group_name}`);
   try {
     // Hash the passcode using Argon2id (recommended variant)
     const hashed = hash(passcode);
-
-    const result = await rds.query(
-      `INSERT INTO ml_group (group_id, group_name, group_url, email, passcode) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
-      [group_id, group_name, group_url, email, hashed]
-    );
+    const result = await rds.query(`INSERT INTO ml_group (group_id, group_name, group_url, email, passcode) VALUES ($1, $2, $3, $4, $5) RETURNING *`, [group_id, group_name, group_url, email, hashed]);
     const { passcode: _, ...safeResult } = result.rows[0];
     return res.status(200).json({
       'result': safeResult
     });
-  } catch (error) {
+  }
+  catch (error) {
     console.error('Error creating group:', error);
     return res.status(500).json({ error: 'Failed to create group' });
   }
-}
-
-export const editGroup = async (req: any, res: any) => {
+};
+export const editGroup = async (req, res) => {
   const { group_id, group_name } = req.body;
   console.log(`Editing group with id: ${group_id} and name: ${group_name}`);
-
   // TODO: Implement this.
-
   return res.status(200).json({
     group_id,
     group_name,
   });
-}
-
-export const getTimeline = async (req: any, res: any) => {
+};
+export const getTimeline = async (req, res) => {
   const { group_id } = req.query;
   console.log(`Getting timeline for group with id: ${group_id}`);
-
   // check if group_id exists in database
-  const group_exists = await rds.query(
-    `SELECT * FROM ml_group WHERE group_id = $1`,
-    [group_id]
-  );
+  const group_exists = await rds.query(`SELECT * FROM ml_group WHERE group_id = $1`, [group_id]);
   if (group_exists.rowCount === 0) {
     res.status(400).json({ error: 'Group ID does not exist.' });
     return;
   }
-
   const group = group_exists.rows[0];
-
   // get all photos for this group
-  const photos = await rds.query(
-    `SELECT photo_url, photo_title, photo_date, photo_caption FROM ml_photos WHERE group_id = $1`,
-    [group_id]
-  );
-
+  const photos = await rds.query(`SELECT photo_url, photo_title, photo_date, photo_caption FROM ml_photos WHERE group_id = $1`, [group_id]);
   res.status(200).json({
     group_name: group.group_name,
     group_url: group.group_url,
     "photo_entries": photos.rows,
     friends: {}
   });
-}
-
-export const uploadPhoto = async (req: any, res: any) => {
+};
+export const uploadPhoto = async (req, res) => {
   const { file } = req;
   const { group_id, photo_title, photo_date, photo_caption } = req.body;
-
   if (!file) {
     return res.status(400).json({ error: 'Photo is required' });
   }
-
   if (!group_id || !photo_title || !photo_date || !photo_caption) {
     return res.status(400).json({ error: 'missing required fields.' });
   }
-
   // check if group_id exists in database
-  const group_exists = await rds.query(
-    `SELECT * FROM ml_group WHERE group_id = $1`,
-    [group_id]
-  );
+  const group_exists = await rds.query(`SELECT * FROM ml_group WHERE group_id = $1`, [group_id]);
   if (group_exists.rowCount === 0) {
     res.status(400).json({ error: 'Group ID does not exist.' });
     return;
   }
-
   console.log('Uploading photo to s3...');
   try {
     const photo_url = await uploadImageToS3(file.buffer, file.originalname, file.mimetype);
-
-    const result = await rds.query(
-      `INSERT INTO ml_photos (group_id, photo_url, photo_title, photo_date, photo_caption) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
-      [group_id, photo_url, photo_title, photo_date, photo_caption]
-    );
-
+    const result = await rds.query(`INSERT INTO ml_photos (group_id, photo_url, photo_title, photo_date, photo_caption) VALUES ($1, $2, $3, $4, $5) RETURNING *`, [group_id, photo_url, photo_title, photo_date, photo_caption]);
     if (result.rowCount === 0) {
       res.status(400).json({ error: 'Failed to upload photo to database.' });
       return;
     }
-
     const group = group_exists.rows[0];
     res.status(200).json({
       message: 'Photo uploaded successfully',
@@ -157,23 +121,21 @@ export const uploadPhoto = async (req: any, res: any) => {
       photo_date: photo_date,
       photo_caption: photo_caption,
     });
-  } catch (error) {
+  }
+  catch (error) {
     console.error('Error uploading photo:', error);
     res.status(500).json({ error: 'Failed to upload photo to s3.' });
   }
-}
-
-async function uploadImageToS3(buffer: Buffer, fileName: string, mimeType: string) {
+};
+async function uploadImageToS3(buffer, fileName, mimeType) {
   const fileExtension = fileName.split('.').pop() || '';
   const newFileName = `${uuidv4()}.${fileExtension}`;
-
   const params = {
-    Bucket: process.env.NODE_AWS_S3_BUCKET!,
+    Bucket: process.env.NODE_AWS_S3_BUCKET,
     Key: newFileName, // Using the new filename instead of original
     Body: buffer,
     ContentType: mimeType,
     ACL: 'public-read',
   };
-
   return (await s3.upload(params).promise()).Location;
 }
--- server/api/index.js
+++ server/api/index.js
@@ -4,22 +4,16 @@ import { initializeDB } from './rds.js';
 import { createGroup, editGroup, getTimeline, upload, uploadPhoto } from './api.js';
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
 app.post('/api/create-group', createGroup);
 app.post('/api/edit-group', editGroup);
 app.post('/api/upload-photo', upload.single('photo'), uploadPhoto);
 app.get('/api/get-timeline', getTimeline);
 
-// Initialize database
-if (!await initializeDB()) {
-  console.error('Failed to initialize database');
-  process.exit(1);
-}
-
-// Start the server
-const PORT = 3005;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+initializeDB();
+// Export default to make Vercel work with serverless functions
+export default app;
